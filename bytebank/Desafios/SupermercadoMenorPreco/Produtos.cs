c632f33e0244669243774b8246a2e2f143b8e92b
rqlalvs <raquelalves3753@gmail.com>
2022-07-13 18:57:36 -0300

    Exercicio - MercadoBomPreco

---
+++ bytebank/Desafios/SupermercadoMenorPreco/Produtos.cs
@@ -0,0 +1,15 @@
+﻿namespace MenorPreco
+{
+    public class Produtos
+    {
+        public string fabricante;
+        public string codigo_barras;
+        public string codigo_produto;
+        public string nome_produto;
+        public int quantidade;
+        public double preco_venda;
+        public double preco_compra;
+        public bool perecivel;
+        public DateOnly validade;
+    }
+}
