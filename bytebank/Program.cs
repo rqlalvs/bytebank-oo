d61cb3bd8c77ce06401523f364fe2524f792bcf9
rqlalvs <raquelalves3753@gmail.com>
2022-07-13 18:41:21 -0300

    Avancos da primeira aula

---
+++ bytebank/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Boas Vindas ao seu banco, ByteBank!");
+
+string titular = "Raquel Alves";
+string conta = "12345-6";
+int numero_agencia = 123;
+string nome_agencia = "Agencia Central";
+double saldo = 100.00;
+
+Console.WriteLine("Titular: " + titular);
+Console.WriteLine("Conta: " + conta);
+Console.WriteLine("Agencia: " + nome_agencia);
+Console.WriteLine("Numero Agencia: " + numero_agencia);
+Console.WriteLine("Saldo: " + saldo);
+
+Console.ReadKey();
